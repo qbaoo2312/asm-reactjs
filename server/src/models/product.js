079509fb8f07cdcc96cdf380d7952c7af1cda67f
desired23 <huydang2312003@gmail.com>
2023-08-07 15:42:13 +0700

    server

---
+++ server/src/models/product.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import Category from "./category";
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      minLength: 3,
+    },
+    price: Number,
+    description: String,
+    images: [String],
+    categoryId: [{
+      type: mongoose.Types.ObjectId,
+      ref: Category,
+    }],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Product", productSchema);
---
+++ server/src/schemas/product.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const productSchema = Joi.object({
+  name: Joi.string().required().min(3),
+  price: Joi.number().required(),
+  description: Joi.string(),
+  images: Joi.array().items(Joi.string()),
+  categoryId: Joi.array().items(Joi.string()).required(),
+});
